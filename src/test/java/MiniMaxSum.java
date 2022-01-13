6e81ea1b7b89d277fe61cfd09402aef560410f2b
serhatozdursun <serhat.ozdursun@gmail.com>
2022-01-13 09:12:59 +0300

    MiniMaxSum

--- src/test/java/MiniMaxSum.java
+++ src/test/java/MiniMaxSum.java
@@ -8,11 +8,11 @@ public class MiniMaxSum {
 
     public static void miniMaxSum(List<Integer> arr) {
         var sortedList = arr.stream().sorted().collect(Collectors.toList());
-        var maxValueList = sortedList.subList(1, 5)
+        var maxValueList = sortedList.subList(1,sortedList.size())
                 .stream()
                 .map(BigInteger::valueOf)
                 .reduce(BigInteger::add);
-        var minValueList = sortedList.subList(0, 4)
+        var minValueList = sortedList.subList(0, sortedList.size()-1)
                 .stream()
                 .map(BigInteger::valueOf)
                 .reduce( BigInteger::add);
